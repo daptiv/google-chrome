880d99d234d048031a144a107d9baf07471d5d36
Ian Gantt <igantt@daptiv.com>
2013-09-09 13:33:11 -0700

    Set chrome as the default browser and add to the environment path

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,14 @@
-default['google-chrome']['track'] = 'stable'
+node.default['google-chrome']['track'] = 'stable'
 
-default['google-chrome']['windows']['package_name'] = 'Google Chrome'
-default['google-chrome']['windows']['url'] =
+node.default['google-chrome']['windows']['package_name'] = 'Google Chrome'
+node.default['google-chrome']['windows']['url'] =
   'https://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi'
 
-default['google-chrome']['windows']['checksum'] =
+node.default['google-chrome']['windows']['checksum'] =
   '5758598caec389a690acc3f29958f3af2cbb087a266f5e151321a477eaba59b4'
+ 
+# Add the command 'chrome' to the environment path for command line execution 
+node.default['google-chrome']['windows']['add_to_path'] = true
+
+# Set Google Chrome as the default browser
+node.default['google-chrome']['windows']['default_browser'] = true
--- recipes/windows.rb
+++ recipes/windows.rb
@@ -13,3 +13,17 @@ windows_package node['google-chrome']['windows']['package_name'] do
   installer_type :msi
   action :install
 end
+
+# Add command to path so it can be launched from the command line
+windows_path 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' do
+  only_if { node['google-chrome']['windows']['add_to_path'] }
+  action :add 
+end
+
+# Set Chrome as the default web browser
+windows_batch 'Make Chrome default browser' do
+  only_if { node['google-chrome']['windows']['default_browser'] }
+  code <<-EOH
+    "%PROGRAMFILES%\\Google\\Chrome\\Application\\chrome.exe" /make-default-browser
+  EOH
+end
