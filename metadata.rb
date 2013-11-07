bb2317e7233d4ba27568c8936756f5e835c5a580
Shawn Neal <sneal@sneal.net>
2013-11-06 22:19:10 -0800

    Upgrade to FoodCritic 3
    Add gemfile.lock and refactor Rakefile
    Changed Windows MSI to download using http instead of https

--- metadata.rb
+++ metadata.rb
@@ -5,10 +5,9 @@ license          'All rights reserved'
 description      'Installs/Configures Google Chrome'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          (ENV['BUILD_NUMBER'] ? "0.1.#{ENV['BUILD_NUMBER']}" : '0.1.3')
-#version          '0.1.0'
 supports         'windows'
 depends          'windows', '>= 1.2.6'
-{"ubuntu" => '12.04', "fedora" => '17.0'}.each do |os,ver|
+{ "ubuntu" => '12.04', "fedora" => '17.0' }.each do |os, ver|
   supports os, ">= #{ver}"
   case os
   when 'fedora'
