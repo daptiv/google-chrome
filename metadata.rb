216864252f6ff75368780ffbeb329615eafe0c9e
Shawn Neal <sneal@sneal.net>
2014-01-30 12:44:54 -0800

    This cookbook's default version starts at 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures Google Chrome'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 ver_path = File.join(File.dirname(__FILE__), 'version.txt')
-version ((IO.read(ver_path) if File.exists?(ver_path)) || '0.0.1').chomp
+version ((IO.read(ver_path) if File.exists?(ver_path)) || '0.1.0').chomp
 supports         'windows'
 depends          'windows', '>= 1.2.6'
 { "ubuntu" => '12.04', "fedora" => '17.0' }.each do |os, ver|
