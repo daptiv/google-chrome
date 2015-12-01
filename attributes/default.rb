e54a5f36cb3fac685b23d64d404901f151140941
Shawn Neal <sneal@sneal.net>
2015-12-01 14:02:58 -0800

    Do not supply a default Windows MSI checksum
    This allows the cookbook to autofloat the latest releast of Chrome without needing to update the checksum attribute.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,3 @@ default['google-chrome']['track'] = 'stable'
 default['google-chrome']['windows']['package_name'] = 'Google Chrome'
 default['google-chrome']['windows']['url'] =
   'http://dl.google.com/edgedl/chrome/install/googlechromestandaloneenterprise64.msi'
-
-default['google-chrome']['windows']['checksum'] =
-  '92f77fd6f9d009845f7b976603655cbeb458e8056dda23749f3d1824e901f032'
