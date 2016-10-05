6316ec9518cc1dac6fcd6822d9c55d3c5cc7b110
heathsnow <heath.snow@changepoint.com>
2016-10-05 13:44:38 -0700

    rubocop update

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description 'Installs/Configures Google Chrome'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 ver_path = File.join(File.dirname(__FILE__), 'version.txt')
 version File.exist?(ver_path) ? IO.read(ver_path).chomp : '1.0.0'
+issues_url 'https://github.com/daptiv/google-chrome/issues'
+source_url 'https://github.com/daptiv/google-chrome/'
 supports 'windows'
 depends 'windows', '>= 1.2.6'
 { 'ubuntu' => '12.04', 'fedora' => '17.0' }.each do |os, ver|
